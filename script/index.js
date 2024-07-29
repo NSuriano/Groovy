5cc202d91096cf9c7ec7bf91d3591a3fe30b82ec
Norma Suriano <surii2908@gmail.con>
2024-07-29 05:39:49 -0300

    primera carga

---
+++ script/index.js
@@ -0,0 +1,16 @@
+
+const asideMenu = document.querySelector("#menu");
+const boton = document.querySelector("#boton");
+const body = document.querySelector("#body");
+const navbar= document.querySelector("#navbar");
+const parrafo = document.getElementsByTagName("[p]");
+
+boton.addEventListener("click", menuLateral);
+
+function menuLateral() {
+    body.classList.toggle("body__move");
+    navbar.classList.toggle("body__move");
+    asideMenu.classList.toggle("menu__lateral");
+    
+    
+}
