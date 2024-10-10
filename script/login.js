6fb694a956201dac4f8e8f32b244a351aa69e528
Norma Suriano <surii2908@gmail.con>
2024-10-10 04:16:18 -0300

    agregando popup a perfil

--- script/login.js
+++ script/login.js
@@ -10,7 +10,7 @@ var tamañoNavegador = window.innerWidth
 btnLogin.addEventListener('click', () =>{
     popupLogin.showModal();
 })
-btnRegister.addEventListener('click',() => {Ñ
+btnRegister.addEventListener('click',() => {
     popupRegister.showModal();
 })
 btnCloseDialog.forEach((btn) => btn.addEventListener('click', () =>{
